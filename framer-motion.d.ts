93a8c5f8d66de18839b200f1f5ab462eb0aaf343
unknown <varshaalluril38@gmail.com>
2026-02-21 18:27:09 +0530

    Improve TypeScript declarations for framer-motion

--- framer-motion.d.ts
+++ framer-motion.d.ts
@@ -1,1 +1,9 @@
-declare module 'framer-motion';
+declare module 'framer-motion' {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const motion: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const AnimatePresence: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const isZeroValueString: any;
+  export { motion, AnimatePresence, isZeroValueString };
+}

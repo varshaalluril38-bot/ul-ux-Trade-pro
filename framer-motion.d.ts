0f886cf56b5a030c1497df2fa0211e4f9952e25f
unknown <varshaalluril38@gmail.com>
2026-02-21 18:27:32 +0530

    Fix framer-motion type declarations

--- framer-motion.d.ts
+++ framer-motion.d.ts
@@ -1,9 +1,37 @@
 declare module 'framer-motion' {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const motion: any;
+  export const motion: any;
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const AnimatePresence: any;
+  export const AnimatePresence: any;
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const isZeroValueString: any;
-  export { motion, AnimatePresence, isZeroValueString };
+  export const isZeroValueString: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export const Variants: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export const useMotionTemplate: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export const useMotionValue: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export const useTransform: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export function useViewportScroll(): any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export function useAnimation(): any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export function useInView(): any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export function useInstantLayoutTransition(): void;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export function useMotionValueEvent(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    motionValue: any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    event: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    callback: any
+  ): void;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export const domAnimation: any;
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  export const domMax: any;
 }
